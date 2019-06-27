91298d27ac09b163eb7baa754cfbf7445535e89e
dish1010 <>
2019-06-27 11:51:09 +0300

    Личный проект: подробности

--- js/main.js
+++ js/main.js
@@ -17,6 +17,14 @@ var pinTemplate = document.querySelector('#pin').content.querySelector('.map__pi
 var mapPins = document.querySelector('.map__pins');
 var mapViewportOffset = adMap.getBoundingClientRect();
 
+var mainPin = mapPins.querySelector('.map__pin--main');
+var adNoties = document.querySelector('.notice');
+var adForm = adNoties.querySelector('.ad-form');
+var adFormFields = document.querySelectorAll('fieldset');
+
+var mainPinAddress = adForm.querySelector('#address');
+var mainPinViewportOffset = mainPin.getBoundingClientRect();
+
 var xMax = mapViewportOffset.right - mapViewportOffset.left - PIN_WIDTH;
 var xMin = 0;
 var yMax = PIN_Y_MAX - PIN_HEIGHT;
@@ -29,7 +37,7 @@ var getRandomValue = function (dataList) {
 };
 
 var getPinLocation = function (min, max) {
-  return Math.floor(Math.random() * (max - min) + min);
+  return Math.random() * (max - min) + min;
 };
 
 var createMapElement = function (elementData) {
@@ -78,7 +86,39 @@ var getMapItems = function (itemsCount) {
   return newMapItems;
 };
 
-adMap.classList.remove('map--faded');
+// нахождение значения поля адреса
+var getMainPinLocation = function () {
+  return mainPinViewportOffset.left + mainPinViewportOffset.width / 2 - mapViewportOffset.left;
+};
 
-var mapItems = getMapItems(ITEMS_COUNT);
-mapPins.appendChild(createMapFragment(mapItems));
+// блокировка элементов формы ввода
+var disableFormElements = function () {
+  for (var i = 0; i < adFormFields.length; i++) {
+    adFormFields[i].setAttribute('disabled', 'disabled');
+  }
+};
+
+// aктивация форм и карты
+var activateElements = function () {
+  for (var i = 0; i < adFormFields.length; i++) {
+    adFormFields[i].removeAttribute('disabled');
+  }
+
+  adMap.classList.remove('map--faded');
+  adForm.classList.remove('ad-form--disabled');
+
+  var mapItems = getMapItems(ITEMS_COUNT);
+  mapPins.appendChild(createMapFragment(mapItems));
+};
+
+disableFormElements();
+
+mainPinAddress.value = getMainPinLocation();
+
+mainPin.addEventListener('click', function () {
+  activateElements();
+});
+
+mainPin.addEventListener('mouseup', function () {
+  mainPinAddress.value = getMainPinLocation();
+});
