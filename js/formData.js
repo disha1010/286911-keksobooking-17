5b05dbecbaa562ab0c4befe940c395d50c529d23
disha1010 <diana.saginbaeva@gmail.com>
2019-08-05 22:41:58 +0300

    Доработка загрузки картинок в форму и селектора выбора комнат

--- js/formData.js
+++ js/formData.js
@@ -1,6 +1,13 @@
 'use strict';
 
 (function () {
+  var FILE_TYPES = ['gif', 'jpg', 'jpeg', 'png'];
+
+  var ROOMS_100 = ['не для гостей'];
+  var ROOMS_1 = ['для 1 гостя'];
+  var ROOMS_2 = ['для 1 гостя', 'для 2 гостей'];
+  var ROOMS_3 = ['для 1 гостя', 'для 2 гостей', 'для 3 гостей'];
+
   var housingType = window.adForm.querySelector('#type');
   var timein = window.adForm.querySelector('#timein');
   var timeout = window.adForm.querySelector('#timeout');
@@ -9,10 +16,12 @@
   var roomNumber = window.adForm.querySelector('#room_number');
   var capacity = window.adForm.querySelector('#capacity');
 
-  var adAvatar = window.adForm.querySelector('#avatar');
-  var adImagesContainer = window.adForm.querySelector('#images');
-  var imgAvatar = document.querySelector('.ad-form-header__preview');
-  var imgPhoto = document.querySelector('.ad-form__photo');
+
+  var avatarFileChooser = document.querySelector('#avatar');
+  var avatarPreview = document.querySelector('.ad-form-header__preview img');
+
+  var imagesFileChooser = document.querySelector('#images');
+  var adPreview = document.querySelector('.ad-form__photo');
 
   // нахождение минимальной цены за ночь
   var getMinPriceByHousingType = function () {
@@ -48,66 +57,75 @@
     timeSynchronization(timeout, timein);
   });
 
+  var fillCapacity = function (selectedRoomCount, notForGuests) {
+    capacity.innerHTML = '';
+
+    selectedRoomCount.forEach(function (optionText, index) {
+      var capacityOption = document.createElement('option');
+      capacity.value = index + 1;
+      if (notForGuests) {
+        capacity.value = index;
+      }
+      capacityOption.innerHTML = optionText;
+      capacity.add(capacityOption);
+    });
+  };
+
   // add capacity option by rooms
   roomNumber.addEventListener('change', function (evt) {
     evt.preventDefault();
-    var roomCount = +roomNumber.value;
-    var capacityOption;
-    capacity.innerHTML = '';
 
-    var room100 = ['не для гостей'];
-    var room1 = ['для 1 гостя'];
-    var room2 = ['для 1 гостя', 'для 2 гостей'];
-    var room3 = ['для 1 гостя', 'для 2 гостей', 'для 3 гостей'];
-
-    var fillCapacity = function (selectedRoomCount, notForGuests) {
-      selectedRoomCount.forEach(function (optionText, index) {
-        capacityOption = document.createElement('option');
-        capacity.value = index + 1;
-        if (notForGuests) {
-          capacity.value = index;
-        }
-        capacityOption.innerHTML = optionText;
-        capacity.add(capacityOption);
-      });
-    };
-
-    switch (roomCount) {
-      case 100:
-        fillCapacity(room100, true);
+    switch (roomNumber.value) {
+      case '100':
+        fillCapacity(ROOMS_100, true);
         break;
-      case 3:
-        fillCapacity(room3);
+      case '3':
+        fillCapacity(ROOMS_3);
         break;
-      case 2:
-        fillCapacity(room2);
+      case '2':
+        fillCapacity(ROOMS_2);
         break;
-      case 1:
-        fillCapacity(room1);
+      case '1':
+        fillCapacity(ROOMS_1);
         break;
     }
   });
 
-  var imageLoader = function (loaderInput, preview) {
-    while (preview.firstChild) {
-      preview.removeChild(preview.firstChild);
-    }
-
-    var curFiles = loaderInput.files;
-    for (var i = 0; i < curFiles.length; i++) {
-      var image = document.createElement('img');
-      image.src = window.URL.createObjectURL(curFiles[i]);
-      image.width = 70;
-      image.height = 70;
-      preview.appendChild(image);
-    }
+  var isFileImg = function (file) {
+    var fileName = file.name.toLowerCase();
+    return FILE_TYPES.some(function (it) {
+      return fileName.endsWith(it);
+    });
   };
 
-  adAvatar.addEventListener('change', function () {
-    imageLoader(adAvatar, imgAvatar);
+  var renderNewImg = function (preview, file) {
+    var reader = new FileReader();
+
+    reader.addEventListener('load', function () {
+      preview.src = reader.result;
+    });
+
+    reader.readAsDataURL(file);
+  };
+
+  avatarFileChooser.addEventListener('change', function () {
+    var file = avatarFileChooser.files[0];
+
+    if (isFileImg(file)) {
+      renderNewImg(avatarPreview, file);
+    }
   });
 
-  adImagesContainer.addEventListener('change', function () {
-    imageLoader(adImagesContainer, imgPhoto);
+  imagesFileChooser.addEventListener('change', function () {
+    Array.from(imagesFileChooser.files).forEach(function (file) {
+      if (isFileImg(file)) {
+        var adPreviewImg = document.createElement('img');
+        adPreviewImg.width = 70;
+        adPreviewImg.height = 70;
+        renderNewImg(adPreviewImg, file);
+
+        adPreview.appendChild(adPreviewImg);
+      }
+    });
   });
 })();
